6d2f7be54337e319bb974060ea46d7838cd033c6
raul reymundi <kinskispiral@gmail.com>
2022-06-05 11:56:10 -0400

    commit tree e32c7286bdc149a32d717b3c0f33feecd0c91cdf

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  // convert string to an array of letters
+    const stringLetters = string.split('');
+  // reverse the array of letters
+    const reverseStringLetters = stringLetters.reverse();
+  // convert array to string
+    const reverseString = reverseStringLetters.join('');
+  // checks whether original string is the same as reversed string
+    if(string == reverseString) {
+      return true;
+  }
+  else {
+      return false;
+  }
 }
 
 /* 
-  Add your pseudocode here
+  write function isPalindrome that receives a string
+  check string letters by turning the letters into strings
+  return true if palindrome (i.e. string/letters are the same no matter the direction)
+  return false if not a palindrome
 */
 
 /*
